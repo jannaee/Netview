92f987dc882a17a8575098b5ee5e7d42dc69a92d
Jannaee Sick <jsick@mbp-jsic.medthink.local>
2019-08-31 18:42:40 -0400

    feat: modal and ui updadte

--- dev-server.js
+++ dev-server.js
@@ -1,55 +1,55 @@
-const {createServer} = require('http');
-const {createProxyServer} = require('http-proxy');
-const Path = require('path');
-const Bundler = require('parcel-bundler');
+// const {createServer} = require('http');
+// const {createProxyServer} = require('http-proxy');
+// const Path = require('path');
+// const Bundler = require('parcel-bundler');
 
 
-const backEnd = {
-	protocol: 'http',
-	host : 'localhost',
-	port: 8080
-};
+// const backEnd = {
+// 	protocol: 'http',
+// 	host : 'localhost',
+// 	port: 8080
+// };
 
 
-const parcelEnd = {
-	protocol: 'http',
-	host: 'localhost',
-	port: 1235
-};
+// const parcelEnd = {
+// 	protocol: 'http',
+// 	host: 'localhost',
+// 	port: 1235
+// };
 
 
-const option = {};
+// const option = {};
 
 
-const entryFiles = Path.join(__dirname, 'src', 'index.html');
+// const entryFiles = Path.join(__dirname, 'src', 'index.html');
 
-const bundler = new Bundler(entryFiles, option);
+// const bundler = new Bundler(entryFiles, option);
 
-bundler.serve();
-
-
-//create a proxy server instance
-const proxy = createProxyServer();
+// bundler.serve();
 
 
-//server
-const server = createServer((req, res)=>{
-	if (req.url.includes('./data/en_US.json')){
-		 proxy.web(req, res, {
-      // back-end server, local tomcat or otherwise
-      target: backEnd,
-      changeOrigin: true,
-      autoRewrite: true
-    });
-  } else {
-    // parcel's dev server
-    proxy.web(req, res, {
-      target: parcelEnd,
-      ws: true
-    });
-  }
-});
+// //create a proxy server instance
+// const proxy = createProxyServer();
 
 
-console.log('Dev Proxy server on: http://localhost:5050');
-server.listen(5050);
+// //server
+// const server = createServer((req, res)=>{
+// 	if (req.url.includes('./data/en_US.json')){
+// 		 proxy.web(req, res, {
+//       // back-end server, local tomcat or otherwise
+//       target: backEnd,
+//       changeOrigin: true,
+//       autoRewrite: true
+//     });
+//   } else {
+//     // parcel's dev server
+//     proxy.web(req, res, {
+//       target: parcelEnd,
+//       ws: true
+//     });
+//   }
+// });
+
+
+// console.log('Dev Proxy server on: http://localhost:5050');
+// server.listen(5050);
